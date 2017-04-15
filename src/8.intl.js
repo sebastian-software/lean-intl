e7388cc8b1293f39e7291d5dd906c82608066dc5
Sebastian Werner <s.werner@sebastian-software.de>
2017-04-15 14:53:31 +0200

    Updated implementation to sync with Intl.js to implement Intl.Pluralrules

--- src/8.intl.js
+++ src/8.intl.js
@@ -1,35 +1,36 @@
-import { CanonicalizeLocaleList } from "./9.negotiation.js"
+import {
+    CanonicalizeLocaleList
+} from "./9.negotiation.js";
 
 // 8 The Intl Object
-export const Intl = {}
+export const Intl = {};
 
 // 8.2 Function Properties of the Intl Object
 
 // 8.2.1
 // @spec[tc39/ecma402/master/spec/intl.html]
 // @clause[sec-intl.getcanonicallocales]
-function getCanonicalLocales(locales) {
-  // 1. Let ll be ? CanonicalizeLocaleList(locales).
-  let ll = CanonicalizeLocaleList(locales)
+function getCanonicalLocales (locales) {
+    // 1. Let ll be ? CanonicalizeLocaleList(locales).
+    let ll = CanonicalizeLocaleList(locales);
+    // 2. Return CreateArrayFromList(ll).
+    {
+        let result = [];
 
-  // 2. Return CreateArrayFromList(ll).
-  {
-    let result = []
+        let len = ll.length;
+        let k = 0;
 
-    let len = ll.length
-    let k = 0
-
-    while (k < len) {
-      result[k] = ll[k]
-      k++
+        while (k < len) {
+            result[k] = ll[k];
+            k++;
+        }
+        return result;
     }
-    return result
-  }
 }
 
-Object.defineProperty(Intl, "getCanonicalLocales", {
+Object.defineProperty(Intl, 'getCanonicalLocales', {
   enumerable: false,
   configurable: true,
   writable: true,
   value: getCanonicalLocales
-})
+});
--- src/exp.js
+++ src/exp.js
@@ -5,25 +5,25 @@
 
 // extlang       = 3ALPHA              ; selected ISO 639 codes
 //                 *2("-" 3ALPHA)      ; permanently reserved
-const extlang = "[a-z]{3}(?:-[a-z]{3}){0,2}"
+const extlang = '[a-z]{3}(?:-[a-z]{3}){0,2}';
 
 // language      = 2*3ALPHA            ; shortest ISO 639 code
 //                 ["-" extlang]       ; sometimes followed by
 //                                     ; extended language subtags
 //               / 4ALPHA              ; or reserved for future use
 //               / 5*8ALPHA            ; or registered language subtag
-const language = `(?:[a-z]{2,3}(?:-${extlang})?|[a-z]{4}|[a-z]{5,8})`
+const language = '(?:[a-z]{2,3}(?:-' + extlang + ')?|[a-z]{4}|[a-z]{5,8})';
 
 // script        = 4ALPHA              ; ISO 15924 code
-const script = "[a-z]{4}"
+const script = '[a-z]{4}';
 
 // region        = 2ALPHA              ; ISO 3166-1 code
 //               / 3DIGIT              ; UN M.49 code
-const region = "(?:[a-z]{2}|\\d{3})"
+const region = '(?:[a-z]{2}|\\d{3})';
 
 // variant       = 5*8alphanum         ; registered variants
 //               / (DIGIT 3alphanum)
-const variant = "(?:[a-z0-9]{5,8}|\\d[a-z0-9]{3})"
+const variant = '(?:[a-z0-9]{5,8}|\\d[a-z0-9]{3})';
 
 //                                     ; Single alphanumerics
 //                                     ; "x" reserved for private use
@@ -32,13 +32,13 @@ const variant = "(?:[a-z0-9]{5,8}|\\d[a-z0-9]{3})"
 //               / %x59-5A             ; Y - Z
 //               / %x61-77             ; a - w
 //               / %x79-7A             ; y - z
-const singleton = "[0-9a-wy-z]"
+const singleton = '[0-9a-wy-z]';
 
 // extension     = singleton 1*("-" (2*8alphanum))
-const extension = `${singleton}(?:-[a-z0-9]{2,8})+`
+const extension = singleton + '(?:-[a-z0-9]{2,8})+';
 
 // privateuse    = "x" 1*("-" (1*8alphanum))
-const privateuse = "x(?:-[a-z0-9]{1,8})+"
+const privateuse = 'x(?:-[a-z0-9]{1,8})+';
 
 // irregular     = "en-GB-oed"         ; irregular tags do not match
 //               / "i-ami"             ; the 'langtag' production and
@@ -57,9 +57,9 @@ const privateuse = "x(?:-[a-z0-9]{1,8})+"
 //               / "sgn-BE-FR"
 //               / "sgn-BE-NL"
 //               / "sgn-CH-DE"
-const irregular = "(?:en-GB-oed" +
-  "|i-(?:ami|bnn|default|enochian|hak|klingon|lux|mingo|navajo|pwn|tao|tay|tsu)" +
-  "|sgn-(?:BE-FR|BE-NL|CH-DE))"
+const irregular = '(?:en-GB-oed'
+          + '|i-(?:ami|bnn|default|enochian|hak|klingon|lux|mingo|navajo|pwn|tao|tay|tsu)'
+          + '|sgn-(?:BE-FR|BE-NL|CH-DE))';
 
 // regular       = "art-lojban"        ; these tags match the 'langtag'
 //               / "cel-gaulish"       ; production, but their subtags
@@ -70,12 +70,12 @@ const irregular = "(?:en-GB-oed" +
 //               / "zh-min"            ; in favor of a more modern
 //               / "zh-min-nan"        ; subtag or sequence of subtags
 //               / "zh-xiang"
-const regular = "(?:art-lojban|cel-gaulish|no-bok|no-nyn" +
-  "|zh-(?:guoyu|hakka|min|min-nan|xiang))"
+const regular = '(?:art-lojban|cel-gaulish|no-bok|no-nyn'
+        + '|zh-(?:guoyu|hakka|min|min-nan|xiang))';
 
 // grandfathered = irregular           ; non-redundant tags registered
 //               / regular             ; during the RFC 3066 era
-const grandfathered = `(?:${irregular}|${regular})`
+const grandfathered = '(?:' + irregular + '|' + regular + ')';
 
 // langtag       = language
 //                 ["-" script]
@@ -83,27 +83,19 @@ const grandfathered = `(?:${irregular}|${regular})`
 //                 *("-" variant)
 //                 *("-" extension)
 //                 ["-" privateuse]
-const langtag = `${language}(?:-${script})?(?:-${region})?(?:-${variant})*(?:-${extension})*(?:-${privateuse})?`
+const langtag = language + '(?:-' + script + ')?(?:-' + region + ')?(?:-'
+        + variant + ')*(?:-' + extension + ')*(?:-' + privateuse + ')?';
 
 // Language-Tag  = langtag             ; normal language tags
 //               / privateuse          ; private use tag
 //               / grandfathered       ; grandfathered tags
-export let expBCP47Syntax = RegExp(
-  `^(?:${langtag}|${privateuse}|${grandfathered})$`,
-  "i"
-)
+export let expBCP47Syntax = RegExp('^(?:'+langtag+'|'+privateuse+'|'+grandfathered+')$', 'i');
 
 // Match duplicate variants in a language tag
-export let expVariantDupes = RegExp(
-  `^(?!x).*?-(${variant})-(?:\\w{4,8}-(?!x-))*\\1\\b`,
-  "i"
-)
+export let expVariantDupes = RegExp('^(?!x).*?-('+variant+')-(?:\\w{4,8}-(?!x-))*\\1\\b', 'i');
 
 // Match duplicate singletons in a language tag (except in private use)
-export let expSingletonDupes = RegExp(
-  `^(?!x).*?-(${singleton})-(?:\\w+-(?!x-))*\\1\\b`,
-  "i"
-)
+export let expSingletonDupes = RegExp('^(?!x).*?-('+singleton+')-(?:\\w+-(?!x-))*\\1\\b', 'i');
 
 // Match all extension sequences
-export let expExtSequences = RegExp(`-${extension}`, "ig")
+export let expExtSequences = RegExp('-'+extension, 'ig');
